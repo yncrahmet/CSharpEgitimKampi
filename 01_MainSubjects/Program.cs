1932379a0c437c5b767569859c66531ec59a2a80
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-10-01 13:07:24 +0300

    int degiskenler

--- 01_MainSubjects/Program.cs
+++ 01_MainSubjects/Program.cs
@@ -79,6 +79,69 @@ namespace _01_MainSubjects
             #region int Değişkenler
 
             // int tam sayı türündeki değişkenlerdir.
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            //int hamburgerPrice = 300;
+            //int cokePrice = 35;
+            //int waterPrice = 10;
+            //int friesPrice = 50;
+            //int pizzaPrice = 250;
+            //int lemonadePrice = 30;
+
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("------Hamburger: " + hamburgerPrice + " TL");
+            //Console.WriteLine("------Pizza: " + pizzaPrice + " TL");
+            //Console.WriteLine("------Kola: " + cokePrice + " TL");
+            //Console.WriteLine("------Su: " + waterPrice + " TL");
+            //Console.WriteLine("------Limonata: " + lemonadePrice + " TL");
+            //Console.WriteLine("------Kızartmalar: " + friesPrice + " TL");
+            //Console.WriteLine();
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+
+            //Console.WriteLine();
+            //int hamburgerCount;
+            //int cokeCount;
+            //int waterCount;
+            //int friesCount;
+            //int pizzaCount;
+            //int lemonadeCount;
+
+            //int totalHamburgerPrice;
+            //int totalCokePrice;
+            //int totalWaterPrice;
+            //int totalFriesPrice;
+            //int totalPizzaPrice;
+            //int totalLemanodePrice;
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //waterCount = 3;
+            //friesCount = 1;
+            //pizzaCount = 0;
+            //lemonadeCount = 0;
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            //totalCokePrice = cokeCount * cokePrice;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalFriesPrice = friesCount * friesPrice;
+            //totalPizzaPrice = pizzaCount * pizzaPrice;
+            //totalLemanodePrice = lemonadeCount * lemonadePrice;
+
+            //Console.WriteLine("-------------------------------------");
+            //Console.WriteLine("Hamburger Tutarı: "+totalHamburgerPrice+" TL");
+            //Console.WriteLine("Kola Tutarı: "+totalCokePrice+" TL");
+            //Console.WriteLine("Su Tutarı: "+totalWaterPrice+" TL");
+            //Console.WriteLine("Kızartma Tutarı: "+totalFriesPrice+" TL");
+            //Console.WriteLine("Pizza Tutarı: "+totalPizzaPrice+" TL");
+            //Console.WriteLine("Limonata Tutarı: "+totalLemanodePrice+" TL");
+            //Console.WriteLine();
+
+            //int totalPrice = totalCokePrice + totalFriesPrice + totalWaterPrice + totalHamburgerPrice + 
+            //    totalLemanodePrice + totalPizzaPrice;
+
+            //Console.WriteLine("Toplam ödenecek tutar: " + totalPrice + " TL");
 
             #endregion
 
