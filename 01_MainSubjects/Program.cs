6f33bf19b0e1fc5c0c36def1a9b36ed2752a328d
Ahmet Yeniçeri <yeniceriahmet02@gmail.com>
2024-10-01 12:42:43 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region YazdırmaKomutları
+
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.Write("Selam");
+
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Çorbalar");
+            //Console.WriteLine("2-Ana yemekler");
+            //Console.WriteLine("3-Soğuk başlangıçlar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-Tatlılar");
+            //Console.WriteLine("6-İçecekler");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+
+            #endregion
+
+            #region String Değişkenler
+
+            // String değişkenler
+            // degisken_türü degisken_adi;
+
+            //string name;
+            //name = "Ahmet";
+            //Console.Write(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, customerDistrict, customerCity;
+
+            //customerName = "Ahmet";
+            //customerSurname = "Yeniçeri";
+            //customerPhone = "+90 555 555 55 55";
+            //customerEmail = "deneme@mail.com";
+            //customerDistrict = "Payas";
+            //customerCity = "Hatay";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + customerDistrict + "/" + customerCity);
+            //Console.WriteLine("--------------------------------------------------");
+
+            //customerName = "Ali";
+            //customerSurname = "Kaya";
+            //customerPhone = "+90 444 444 44 44";
+            //customerEmail = "test@mail.com";
+            //customerDistrict = "Kadiköy";
+            //customerCity = "İstanbul";
+
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + customerDistrict + "/" + customerCity);
+            //Console.WriteLine("--------------------------------------------------");
+
+            #endregion
+
+            #region int Değişkenler
+
+            // int tam sayı türündeki değişkenlerdir.
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+// Yazdırma Komutları
